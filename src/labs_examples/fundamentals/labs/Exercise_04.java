54aee0c0523a72dab6013353d58e4542dac0c791
matthewmotley <matthew.d.motley@gmail.com>
2023-04-29 11:01:52 -0500

    primitive work

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,7 +16,29 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean none = false;
+        System.out.println("Is today Sunday? "+ none);
 
+        char letter = 'J';
+        System.out.println("The 10th letter of the alphabet is: "+ letter);
+
+        byte byteNum = 123;
+        System.out.println("My favorite number between -128 and 127 is: "+ byteNum);
+
+        short shorty = 12623;
+        System.out.println("There are " + shorty + " residence in my hometown.");
+
+        int chance = 1000000;
+        System.out.println("The odds are "+ chance + ":1");
+
+        long limit = 9223372036854775807l;
+        System.out.println(limit + " Now that's a LONG number!");
+
+        float floating = 93485709348752093452340985723495f;
+        System.out.println("I could FLOAT for less than " + floating + " minutes");
+
+        double percentSure = 99.996934845723459902372349934577;
+        System.out.println("I'm " + percentSure + "% sure you'll be ok.");
     }
 
 }
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -1,6 +1,8 @@
 package labs_examples.fundamentals.labs;
 
 
+import java.net.SocketOption;
+
 /**
  * Fundamentals Exercise 5: Working with Strings
  *
@@ -14,14 +16,22 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int x = str.length();
+        System.out.println("The string length is: "+x);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        int y =str2.length();
+        boolean bool = x==y;
+        System.out.println("Is str equal to str2? " + bool);
         // please concatenate str & str2 and set the result to a new String variable below
-
+        System.out.println(str+" "+str2);
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        System.out.println("A big " + str2.toUpperCase() + " to you!");
+        String substr = "";
+        substr = str.substring(4,6);
+        System.out.println(substr);
 
     }
 
